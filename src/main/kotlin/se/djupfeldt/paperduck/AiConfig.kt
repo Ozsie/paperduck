48b6f4d403176eacf121656626c3bcb4b3e65c55
Oscar Djupfeldt <oscar.djupfeldt@abilia.se>
2026-06-21 19:07:44 +0200

    Log OpenAI and Mistral AI chat client configurations

--- src/main/kotlin/se/djupfeldt/paperduck/AiConfig.kt
+++ src/main/kotlin/se/djupfeldt/paperduck/AiConfig.kt
@@ -1,5 +1,6 @@
 package se.djupfeldt.paperduck
 
+import org.slf4j.LoggerFactory
 import org.springframework.ai.chat.client.ChatClient
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor
 import org.springframework.ai.mistralai.MistralAiChatModel
@@ -10,16 +11,20 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class AiConfig {
-
+    private val log = LoggerFactory.getLogger(AiConfig::class.java)
     @Bean
     @ConditionalOnProperty(name = ["paperduck.ai.service"], havingValue = "openai")
     fun openAiChatClient(chatModel: OpenAiChatModel): ChatClient = ChatClient.builder(chatModel)
         .defaultAdvisors(SimpleLoggerAdvisor())
-        .build()
+        .build().also {
+            log.info("Using OpenAI chat client")
+        }
 
     @Bean
     @ConditionalOnProperty(name = ["paperduck.ai.service"], havingValue = "mistral", matchIfMissing = true)
     fun mistralAiChatClient(chatModel: MistralAiChatModel): ChatClient = ChatClient.builder(chatModel)
         .defaultAdvisors(SimpleLoggerAdvisor())
-        .build()
+        .build().also {
+            log.info("Using Mistral AI chat client")
+        }
 }
